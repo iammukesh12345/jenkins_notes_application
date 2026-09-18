ced7b8472dd6d609e103c5ef1deb95924225380b
Mukesh Thakre <mukeshthakre015@gmail.com>
2026-09-18 20:21:42 +0000

    All database setup is done

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -89,7 +89,12 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy'
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
